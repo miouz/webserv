02d4967fa80be8a72c975e726a67727c72534caf
skc <sheikouz@gmail.com>
2026-03-17 16:33:04 +0100

    feat(parser-request): add parser-request
    * parse HTTP request
    
    test command: ./test/http-request-parser-test/http_test.sh

---
+++ RequestParser.cpp
@@ -0,0 +1,150 @@
+#include "RequestParser.hpp"
+#include <cstdlib>
+#include <iostream>
+
+RequestParser::RequestParser(): buffer_(""), isStartParsed_(false), isHeadersParsed_(false), isBodyParsed_(false) {}
+
+RequestParser::~RequestParser() {}
+
+void	RequestParser::feed(std::string& chunk)
+{
+	buffer_ += chunk;
+
+	if (isStartParsed_ == false)
+		parseStartLine();
+	else if (isHeadersParsed_ == false)
+		parseHeaders();
+	else if (isBodyParsed_ == false)
+		parseBody();
+}
+
+void	RequestParser::parseStartLine()
+{
+	std::string	EOL("\r\n");
+
+	size_t	found = buffer_.find(EOL);
+	if (found == std::string::npos)
+	{
+		throw std::runtime_error("Out of scope: Time out");//WARNING: tmp: out of scope
+		return ;
+	}
+	std::istringstream	iss(buffer_.substr(0, found));
+	buffer_ = buffer_.substr(found + EOL.size());
+	data_.method = getToken(iss);
+	data_.uri = getToken(iss);
+	data_.protocol = getToken(iss);
+	checkStartLine();
+	isStartParsed_ = true;
+}
+
+void	RequestParser::parseHeaders()
+{
+	std::string	sep = ":";
+	std::string	key;
+	std::string	value;
+
+	if (isHeadersParsed_ == false && buffer_.find("\r\n\r\n") != std::string::npos)
+		isHeadersParsed_ = true;
+	while (1)
+	{
+		std::string	line = getLine(buffer_);
+		if (line.empty())
+			return ;
+		std::size_t	found = line.find(sep);
+		if (found == std::string::npos)
+			value = "";
+		else
+			value = line.substr(found + sep.size());
+		key = line.substr(0, found);
+		setHeader(key, value);
+	}
+}
+
+void	RequestParser::parseBody()
+{
+	std::map<std::string, std::string>::iterator contentLen = data_.headers.find("CONTENT-LENGTH:");
+	if (contentLen != data_.headers.end())
+	{
+		size_t	totalSize = atoi(contentLen->second.c_str());
+		if (buffer_.size() < totalSize)
+			return ;
+		data_.body = buffer_.substr(0, totalSize);
+	}
+	isBodyParsed_ = true;
+}
+
+std::string	RequestParser::getLine(std::string& str) const
+{
+	std::string	EOL = "\r\n";
+	std::size_t	found = str.find(EOL);
+	std::string	line;
+
+	if (found != std::string::npos)
+	{
+		line = str.substr(0, found);
+		str = str.substr(found + EOL.size());
+	}
+	return line;
+}
+
+std::string	RequestParser::getToken(std::istringstream& iss) const
+{
+	std::string	token;
+	iss >> token;
+	return token;
+}
+
+void	RequestParser::setHeader(std::string& key, std::string& value)
+{
+	std::string	token;
+	std::istringstream	iss(value);
+	iss >> value;
+	if (iss >> token)
+		throw std::runtime_error("400 Bad Request");
+	capitalize(key);
+	capitalize(value);
+	checkHeaders(key, value);
+	data_.headers[key] = value;
+}
+
+void	RequestParser::checkStartLine() const
+{
+	static const int	NBR_METHODS = 3;
+	if (data_.protocol != "HTTP/1.0")
+		throw std::runtime_error("400 Bad Request");
+	if (data_.uri[0] != '/')
+		throw std::runtime_error("400 Bad Request");
+
+	std::string	methods[NBR_METHODS] = {"GET", "POST", "DELETE"};
+	for (int i = 0; i < NBR_METHODS; i++)
+		if (data_.method == methods[i])
+			return ;
+	throw std::runtime_error("400 Bad Request");
+}
+
+void	RequestParser::checkHeaders(std::string& key, std::string& value) const
+{
+	if (key.find(" ") != std::string::npos)
+		throw std::runtime_error("400 Bad Request");
+	if (key == "HOST" && value.empty())
+		throw std::runtime_error("400 Bad Request");
+	if (key == "HOST" && data_.headers.find("HOST") != data_.headers.end())
+		throw std::runtime_error("400 Bad Request");
+}
+
+void	RequestParser::capitalize(std::string& str) const
+{
+	for (size_t i = 0; i < str.size(); i++)
+		if (std::islower(str[i]))
+			str[i] -= 'a' - 'A';
+}
+
+bool	RequestParser::isComplete() const
+{
+	return isBodyParsed_;
+}
+
+ParsedData	RequestParser::getData() const
+{
+	return data_;
+}
---
+++ RequestParser.hpp
@@ -0,0 +1,48 @@
+#ifndef REQUEST_PARSER_HPP
+# define REQUEST_PARSER_HPP
+
+#include <map>
+#include <string>
+#include <sstream>
+
+struct ParsedData
+{
+	std::string	method;
+	std::string	uri;
+	std::string	protocol;
+	std::map<std::string, std::string>	headers;
+	std::string	body;
+};
+
+class RequestParser
+{
+	private:
+		ParsedData	data_;
+		std::string	buffer_;
+		bool	isStartParsed_;
+		bool	isHeadersParsed_;
+		bool	isBodyParsed_;
+
+		void	parseStartLine();
+		void	parseHeaders();
+		void	parseBody();
+
+		std::string	getLine(std::string&) const;
+		std::string	getToken(std::istringstream&) const;
+
+		void	setHeader(std::string&, std::string&);
+		void	checkHeaders(std::string&, std::string&) const;
+		void	checkStartLine() const;
+		void	checkBody() const;
+
+		void	capitalize(std::string&) const;
+
+	public:
+		RequestParser();
+		~RequestParser();
+		void	feed(std::string&);
+		bool	isComplete() const;
+		ParsedData	getData() const;
+};
+
+#endif
