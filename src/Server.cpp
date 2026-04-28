cd90a5e366840c18ecf1cab4d81afd1f655ed311
Zhou Mi <mizhouis@gmail.com>
2026-04-28 11:16:22 +0200

    feat(serverEventHandler): change accept client by client by accept all clients once on a server

--- src/Server.cpp
+++ src/Server.cpp
@@ -65,7 +65,11 @@ Client* Server::acceptClient()
 	socklen_t len = sizeof(addr);
 	int fd = accept(sockFd_, (struct sockaddr*)&addr, &len);
 	if (fd == RETURN_ERROR)
+	{
+		if (errno == EWOULDBLOCK || errno == EAGAIN)
+			return NULL;
 		throw std::runtime_error(std::string("can't accept new client: ") + std::strerror(errno));
+	}
 	if (fcntl(fd, F_SETFL, O_NONBLOCK) == RETURN_ERROR
 		|| fcntl(fd, F_SETFD, FD_CLOEXEC) == RETURN_ERROR)
 	{
