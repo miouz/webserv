3d11e49a1ffdaaf7d82188c09499f7d5cf7d4d6c
skc <sheikouz@gmail.com>
2026-05-08 14:35:31 +0200

    chore(cgi): clean log msg

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -194,14 +194,8 @@ void	Cgi::execChild(char** env, int* pipein, int*pipeout)
 	}
 	argv[1] = const_cast<char*>(scriptPath.c_str());
 	argv[2] = NULL;
-	#ifdef DEBUG
-	std::cerr << "EXECVE: " << argv[0] << " | " << argv[1] << '\n';
-#endif
 	execve(argv[0], argv, env);
 	freeEnv(env);
-	#ifdef DEBUG
-	std::cerr << "s'est mal passe \n";
-#endif
 
 	exit(1);
 }
